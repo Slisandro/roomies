9dd40f6992929ed9d3baa882e8027d3f9b5cc786
Lisandro <salvareschilisandro0@gmail.com>
2023-10-12 13:54:29 -0300

    apikey, hooks and contexts stream chat

---
+++ services/chat/config.ts
@@ -0,0 +1,5 @@
+export const chatApiKey = 'jcw6m7hz2r3h';
+export const chatSecret = 'sk4z8rnv77qmx5ecsh32yb2f9qazdy9kdctryreryu77xr9hgu7pf2wgpxntapg9';
+export const chatUserId = '02922ef1_50ce_4ed2_bef0_03a144f1c876';
+// export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic25vd3ktbmlnaHQtNyIsImV4cCI6MTY5Njk4MDczOH0.CTu3Pscnx2QZP4srV8aaG0N0-2jsAZEyOtlYXE8RWtE';
+export const chatUserName = 'Lisandro Salvareschi';
