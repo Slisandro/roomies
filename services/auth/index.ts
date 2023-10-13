77beb5d5da51722572efb3567796e7bfff64e4aa
Lisandro <salvareschilisandro0@gmail.com>
2023-10-12 21:13:28 -0300

    update services auth

--- services/auth/index.ts
+++ services/auth/index.ts
@@ -1,4 +1,4 @@
-import { Auth, createUserWithEmailAndPassword, getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { Auth, UserCredential, createUserWithEmailAndPassword, getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import { Toast } from 'toastify-react-native';
 import { AUTH_ERROR } from "../../constants/auth/error-auth";
 import { AUTH_SUCCESS } from "../../constants/auth/success-auth";
@@ -7,39 +7,53 @@ import { User } from "../../models/user-model";
 
 const AUTH: Auth = auth;
 
-export const serviceLogin = async (email: string, password: string) => {
-    return signInWithEmailAndPassword(AUTH, email, password)
-        .then(r => {
-            Toast.success(AUTH_SUCCESS["auth/login"])
-            return r
-        })
-        .catch(u => Toast.error(AUTH_ERROR[u.code]));
-    // spinner
-    // .finally()
-};
+export interface ResponseService {
+    error: boolean,
+    message?: string,
+    code?: number;
+    data?: UserCredential
+}
 
-export const serviceRegister = async (u: User) => {
-    const { email, password, photoURL, displayName } = u;
-    return createUserWithEmailAndPassword(AUTH, email, password)
-        .then(async r => {
-            const userCreated = getAuth();
-
-            await userCreated.updateCurrentUser({
-                ...userCreated.currentUser,
-                photoURL,
-                displayName
+export const serviceLogin:
+    (e: string, p: string) => Promise<ResponseService>
+    = async (email: string, password: string) =>
+        signInWithEmailAndPassword(AUTH, email, password)
+            .then(r => {
+                Toast.success(AUTH_SUCCESS["auth/login"]);
+                return { data: r, error: false };
+            })
+            .catch(u => {
+                Toast.error(AUTH_ERROR[u.code]);
+                return { error: true, message: AUTH_ERROR[u.code], code: u.code }
             });
 
-            return r
-        })
-        .catch(e => console.log(e))
-};
+export const serviceRegister:
+    (u: User) => Promise<ResponseService>
+    = async (u: User) =>
+        createUserWithEmailAndPassword(AUTH, u.email, u.password)
+            .then(async r => {
+                const userCreated = getAuth();
 
-export const serviceResetPassword = async (email: string) => {
-    try {
-        const r = await sendPasswordResetEmail(AUTH, email);
-        console.log(r);
-    } catch (e) {
-        console.log(e);
-    }
-};
+                await userCreated.updateCurrentUser({
+                    ...userCreated.currentUser,
+                    photoURL: u?.photoURL || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
+                    displayName: u.displayName
+                })
+                    .then(r => console.log({ r }))
+                    .catch(e => console.log({ e }))
+
+                Toast.success(AUTH_SUCCESS["auth/register"]);
+
+                return { data: r, error: false }
+            })
+            .catch(u => {
+                Toast.error(AUTH_ERROR[u.code]);
+                return { error: true, message: AUTH_ERROR[u.code], code: u.code }
+            })
+
+export const serviceResetPassword:
+    (e: string) => void
+    = async (email: string) =>
+        sendPasswordResetEmail(AUTH, email)
+            .then(() => Toast.success(AUTH_SUCCESS["auth/reset-password"]))
+            .catch(u => Toast.success(AUTH_SUCCESS[u.code]));
