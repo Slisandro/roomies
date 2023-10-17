176da7c475dd1092fcfd1c5beb59493fe49a3f87
Lisandro <salvareschilisandro0@gmail.com>
2023-10-17 16:37:47 -0300

    add image services

--- services/rooms/get-rooms.tsx
+++ services/rooms/get-rooms.tsx
@@ -3,20 +3,22 @@ import { RoomPost } from "../../models/post-room-model";
 export const serviceGetRooms = (): RoomPost[] => {
     return [
         {
-            "lon": -60.32974737646605,
-            "lat": -36.91621360789941,
-            "id": "9876543211",
-            "title": "Room 1",
-            "description": "Big room with all accommodations",
-            "date": ""
+            lon: -60.32974737646605,
+            lat: -36.91621360789941,
+            id: "9876543211",
+            title: "Room 1",
+            description: "Big room with all accommodations",
+            date: "",
+            media: ["https://images.unsplash.com/photo-1616594039964-ae9021a400a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJlZHJvb218ZW58MHx8MHx8fDA%3D&w=1000&q=80"]
         },
         {
-            "lon": -60.28974737646605,
-            "lat": -36.91621364780041,
-            "id": "1234567892",
-            "title": "Room 2",
-            "description": "Big room with all accommodations",
-            "date": ""
+            lon: -60.28974737646605,
+            lat: -36.91621364780041,
+            id: "1234567892",
+            title: "Room 2",
+            description: "Big room with all accommodations",
+            date: "",
+            media: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxOANWsrTGXxJVS3E_FczGRSqksFh3wRBQSfvI1qa&s"]
         },
     ];
 }
