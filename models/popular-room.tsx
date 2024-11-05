bcf0ca0facae7decb13f2f0bb83b78736c39b4ca
Lisandro <salvareschilisandro0@gmail.com>
2024-11-05 12:25:24 -0300

    model popular room

---
+++ models/popular-room.tsx
@@ -0,0 +1,10 @@
+export interface PopularRoom {
+    id: string,
+    lon: number,
+    lat: number,
+    title: string,
+    description: string,
+    date: string,
+    media: string[],
+    likes: number
+}
