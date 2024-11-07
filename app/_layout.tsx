5c536238c27c79c7af4f0f693ec316b6593c5d9b
Lisandro <salvareschilisandro0@gmail.com>
2024-11-07 12:56:25 -0300

    fix config layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -47,7 +47,7 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ backgroundColor: "transparent" }}>
+      <Stack>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
